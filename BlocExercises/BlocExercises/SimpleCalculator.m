ab7fb2c7f7c058a038e94403fe4c119b130bfbd7
dman2000 <dnlesp@hotmail.com>
2014-09-07 13:45:07 +0100

    Completed the numbers and variables

--- BlocExercises/BlocExercises/SimpleCalculator.m
+++ BlocExercises/BlocExercises/SimpleCalculator.m
@@ -17,12 +17,12 @@
 
 - (NSInteger) addNumber:(NSInteger) number1 toNumber:(NSInteger) number2 {
     /* WORK HERE */
-    return number1;
+    return number1 + number2;
 }
 
 - (NSInteger) remainderOfNumber:(NSInteger) dividend dividedByNumber:(NSInteger) divisor {
     /* WORK HERE */
-    return 0;
+    return dividend % divisor;
 }
 
 @end
